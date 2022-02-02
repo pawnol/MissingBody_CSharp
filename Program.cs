d06f992cdf09592d5827e42923b4c016d4ec622a
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-02-01 20:37:24 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Missing Body
+ * Pawelski
+ * 2/1/2022
+ * Read over the code and run the program a few times to determine
+ * what the program currently does and how currently it works. Once 
+ * you understand the program, add the code described on the activity
+ * sheet. In addition, answer the question on the activity sheet.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingBody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumbers(7);
+            PrintNumbers(3);
+        }
+
+        // Add your code here!
+    }
+}
